f2c6896233f81d0af037195f39fd390b8d808bc0
Yogeshwar B <40350972+yogeshwar-b@users.noreply.github.com>
2024-05-09 23:58:35 +0530

    Brush hover added (#9)

--- src/LayerBoard.tsx
+++ src/LayerBoard.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { Board } from './panels/Board'
 import { ColorPalette } from './panels/ColorPalette'
 import { LayersPanel } from './panels/LayersPanel'
@@ -5,12 +6,20 @@ import { Toolbox } from './panels/Toolbox'
 import './styles/utils.css'
 
 function LayerBoard() {
+  const [BrushState, changeBrushState] = useState('default')
   return (
     <div className='flex-col height-max'>
       <h1 className='text-center'>LayerBoard</h1>
       <div className='flex-row flex-grow'>
-        <Toolbox className='rounded-1 m-1 p-1 shadow-1 border-small' />
-        <Board className='flex-grow height-auto rounded-1 m-1 p-1 shadow-1 border-small' />
+        <Toolbox
+          className='rounded-1 m-1 p-1 shadow-1 border-small'
+          brushState={BrushState}
+          changeBrushState={changeBrushState}
+        />
+        <Board
+          className='flex-grow height-auto rounded-1 m-1 p-1 shadow-1 border-small'
+          brushState={BrushState}
+        />
         <LayersPanel className='rounded-1 m-1 p-1 shadow-1 border-small' />
       </div>
       <ColorPalette className='palette rounded-1 m-1 p-1 shadow-1 border-small' />
