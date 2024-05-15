200dc9040df64cfbdf571408f7221213899d8d96
Yogeshwar B <40350972+yogeshwar-b@users.noreply.github.com>
2024-05-15 14:23:58 +0530

    Added Radio toolbox buttons (#17)
    * Added Radio toolbox buttons
    
    * Managing the hover class and maintaining the canvas state on brush changes.

--- src/components/ToolButton.tsx
+++ src/components/ToolButton.tsx
@@ -1,20 +1,32 @@
 import '../styles/utils.css'
+import '../styles/toolbutton.css'
+import { Tools } from '../enums/tools'
 
 interface ToolButtonProps {
   name: string
   iconpath: string
+  id: string
+  toolNum: Tools
+  changeToolState: (x: Tools) => void
+  toolState: Tools //Selected Tool
 }
 
 export const ToolButton = (props: ToolButtonProps) => {
   return (
-    <label htmlFor={props.name} title={props.name}>
-      <img src={props.iconpath} alt='some icon' />
+    <label htmlFor={props.id} title={props.name}>
       <input
         className='disp-no'
         type='radio'
-        id={props.name}
-        title={props.name}
+        name={props.name}
+        id={props.id}
+        title={props.id}
+        onChange={(e) => {
+          props.changeToolState(props.toolNum)
+          console.log(`Selected - ${e.currentTarget.title}`)
+        }}
+        checked={props.toolState == props.toolNum}
       />
+      <img src={props.iconpath} alt='some icon' className='icon' />
     </label>
   )
 }
--- src/panels/Toolbox.tsx
+++ src/panels/Toolbox.tsx
@@ -1,4 +1,4 @@
-import { Dispatch, SetStateAction } from 'react'
+import { Dispatch, SetStateAction, useState } from 'react'
 import { Tools } from '../enums/tools'
 import { ToolButton } from '../components/ToolButton'
 import { toolbuttons } from '../constants'
@@ -7,22 +7,64 @@ interface ToolboxProps {
   className: string
   toolState: Tools
   changeToolState: Dispatch<SetStateAction<Tools>>
+  ToolRef: React.MutableRefObject<Tools>
 }
 //@todo - remove button
 export const Toolbox = (props: ToolboxProps) => {
+  const [ToolState, changeToolState]: [Tools, Dispatch<SetStateAction<Tools>>] =
+    useState<Tools>(Tools.None)
+
+  function changeToolState1(x: Tools) {
+    const canvaselement = document.getElementById('canvas-id')
+    switch (props.ToolRef.current) {
+      case Tools.None:
+        canvaselement?.classList.remove('none-hover')
+        break
+      case Tools.Brush:
+        canvaselement?.classList.remove('brush-hover')
+        break
+      case Tools.Eraser:
+        canvaselement?.classList.remove('eraser-hover')
+        break
+      case Tools.Move:
+        canvaselement?.classList.remove('move-hover')
+        break
+      default:
+        break
+    }
+    props.ToolRef.current = x
+    switch (props.ToolRef.current) {
+      case Tools.None:
+        canvaselement?.classList.add('none-hover')
+        break
+      case Tools.Brush:
+        canvaselement?.classList.add('brush-hover')
+        break
+      case Tools.Eraser:
+        canvaselement?.classList.add('eraser-hover')
+        break
+      case Tools.Move:
+        canvaselement?.classList.add('move-hover')
+        break
+      default:
+        break
+    }
+    changeToolState(x)
+  }
   return (
     <div className={props.className}>
       <div>Toolbox</div>
-
-      <button
-        onClick={() => {
-          props.changeToolState(Tools.Brush)
-        }}
-      >
-        Brush
-      </button>
       {toolbuttons.map((t) => {
-        return <ToolButton name={t.name} iconpath={t.iconPath}></ToolButton>
+        return (
+          <ToolButton
+            name='toolbar'
+            id={t.name}
+            iconpath={t.iconPath}
+            toolNum={t.toolNum}
+            changeToolState={changeToolState1}
+            toolState={ToolState}
+          ></ToolButton>
+        )
       })}
     </div>
   )
