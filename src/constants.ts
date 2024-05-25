09991c1ec9659765a7bbc7b76d9767f0589d2207
Yogeshwar B. <byogeshwarreddy@gmail.com>
2024-05-26 00:40:15 +0530

    icons path modified

--- src/constants.ts
+++ src/constants.ts
@@ -9,18 +9,18 @@ interface ToolButton {
 export const toolbuttons: ToolButton[] = [
   {
     name: 'mouse',
-    iconPath: '/LayerBoard/icons/mousearrow.svg',
+    iconPath: './icons/mousearrow.svg',
     toolNum: Tools.None
   },
   {
     name: 'brush',
-    iconPath: '/LayerBoard/icons/brush.svg',
+    iconPath: './icons/brush.svg',
     toolNum: Tools.Brush
   },
   {
     name: 'eraser',
-    iconPath: '/LayerBoard/icons/eraser.svg',
+    iconPath: './icons/eraser.svg',
     toolNum: Tools.Eraser
   },
-  { name: 'move', iconPath: '/LayerBoard/icons/move.svg', toolNum: Tools.Move }
+  { name: 'move', iconPath: './icons/move.svg', toolNum: Tools.Move }
 ]
