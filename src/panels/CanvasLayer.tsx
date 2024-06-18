a01b9d7597ed9b32d95a5b3823490b3138e1a9e8
Yogeshwar B. <byogeshwarreddy@gmail.com>
2024-06-19 00:07:17 +0530

    changed OnDrag back to PointerMove

--- src/panels/CanvasLayer.tsx
+++ src/panels/CanvasLayer.tsx
@@ -5,6 +5,7 @@ import '../styles/svg.css'
 import '../styles/canvaslayer.css'
 var xdef = -1
 var ydef = -1
+var offsetX, offsetY
 interface CanvasLayerProps {
   canvasId: string
   ToolRef: React.MutableRefObject<Tools>
@@ -42,7 +43,7 @@ export const CanvasLayer = ({ canvasId, ToolRef }: CanvasLayerProps) => {
     >
       {MoveToolOverlay.ShowMoveToolOverlay ? (
         <div
-          draggable='true'
+          // draggable='true'
           className='move-overlay'
           style={{
             zIndex: 120,
@@ -58,80 +59,43 @@ export const CanvasLayer = ({ canvasId, ToolRef }: CanvasLayerProps) => {
               .left,
             transform: 'matrix(1, 0, 0, 1, 0, 0)'
           }}
-          onPointerDown={(e: React.MouseEvent) => {
+          onPointerDown={(e) => {
             //True if Tool is not None
             isToolActive.current = ToolRef.current != Tools.None
             if (ToolRef.current == Tools.Move) {
-              xdef = e.clientX
-              ydef = e.clientY
+              let draggableDiv = e.target as HTMLDivElement
+              offsetX = e.clientX - draggableDiv.offsetLeft
+              offsetY = e.clientY - draggableDiv.offsetTop
               setIsMoving(true)
+              console.log('move activated')
             }
           }}
-          onDrag={(e: React.MouseEvent) => {
-            if (ToolRef.current == Tools.Move) {
-              if (isMoving.current) {
-                var x = window.getComputedStyle(
-                  e.target as HTMLDivElement,
-                  null
-                ).transform
-                var numberPattern = /-?\d+\.?\d+|\d+/g
+          onPointerMove={(e: React.MouseEvent) => {
+            if (ToolRef.current == Tools.Move && isMoving.current) {
+              {
+                let draggableDiv = e.target as HTMLDivElement
+                draggableDiv.style.left = `${e.clientX - offsetX}px`
+                draggableDiv.style.top = `${e.clientY - offsetY}px`
 
-                var matrix = x.match(numberPattern)
-                var transx = e.clientX - xdef
-                var transy = e.clientY - ydef
-                xdef = e.clientX
-                ydef = e.clientY
-                if (matrix) {
-                  matrix[4] = String(transx + Number(matrix[4]))
-                  matrix[5] = String(transy + Number(matrix[5]))
-                  ;(
-                    e.target as HTMLDivElement
-                  ).style.transform = `matrix(${matrix.join(',')})`
+                if (MoveToolOverlay.PolyLineRef?.current) {
+                  var numberPattern = /-?\d+\.?\d+|\d+/g
 
-                  if (MoveToolOverlay.PolyLineRef?.current)
-                    MoveToolOverlay.PolyLineRef.current.style.transform = `matrix(${matrix.join(
-                      ','
-                    )})`
+                  var matrix =
+                    MoveToolOverlay.PolyLineRef.current.style.transform.match(
+                      numberPattern
+                    )
+                  matrix[4] = String(Number(e.clientX) - Number(offsetX))
+                  matrix[5] = String(Number(e.clientY) - Number(offsetY))
+                  MoveToolOverlay.PolyLineRef.current.style.transform = `matrix(${matrix.join(
+                    ','
+                  )})`
                 }
               }
             }
           }}
-          onDragEnd={(e: React.MouseEvent) => {
-            if (isToolActive.current && ToolRef.current == Tools.Eraser) {
-              ;(e.target as SVGPolylineElement).points.clear()
-            }
-            if (ToolRef.current == Tools.Move) {
-              if (isMoving.current) {
-                var x = window.getComputedStyle(
-                  e.target as HTMLDivElement,
-                  null
-                ).transform
-                var numberPattern = /-?\d+\.?\d+|\d+/g
-                var matrix = x.match(numberPattern)
-                // console.log(x, window.getComputedStyle(e.target, null))
-                var transx = e.clientX - xdef
-                var transy = e.clientY - ydef
-                xdef = e.clientX
-                ydef = e.clientY
-                if (matrix) {
-                  matrix[4] = String(transx + Number(matrix[4]))
-                  matrix[5] = String(transy + Number(matrix[5]))
-                  ;(
-                    e.target as HTMLDivElement
-                  ).style.transform = `matrix(${matrix.join(',')})`
-                  if (MoveToolOverlay.PolyLineRef?.current)
-                    MoveToolOverlay.PolyLineRef.current.style.transform = `matrix(${matrix.join(
-                      ','
-                    )})`
-                }
-              }
-            }
-          }}
-          onPointerUp={() => {
-            console.log('is moving deactivated')
-            // xdef = -1
-            // ydef = -1
+          onPointerUp={(e: React.MouseEvent) => {
             setIsMoving(false)
+            console.log('is moving deactivated')
           }}
         ></div>
       ) : (
@@ -235,6 +199,7 @@ const PolyLineSVG = ({
           points={points}
           className='poly-line'
           ref={newPolyLineRef}
+          style={{ transform: 'matrix(1, 0, 0, 1, 0, 0)' }}
           name={name}
           onPointerDown={() => {
             //True if Tool is not None
